5be7d348f07288318e4f98a197d50c5bb7514657
Syoyo Fujita <syoyo@lighttransport.com>
2023-06-21 04:59:49 +0900

    Add mindep + monolithic build script.

---
+++ build_scripts/mindep-mono.sh
@@ -0,0 +1,2 @@
+# Use python3.7
+python build_scripts/build_usd.py --no-python --build-monolithic --no-openvdb --no-materialx --no-ptex --no-embree --no-prman --no-hdf5 --no-alembic --no-draco --no-docs --no-imaging --no-openimageio --no-opencolorio  USD-mindep-dist-23_05
